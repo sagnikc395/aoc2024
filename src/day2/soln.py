e8e72371539d1bc21632e21a1857449a0b2e93fc
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-03 10:47:52 +0530

    src: solved part2, day2 and add cli gflags

--- src/day2/soln.py
+++ src/day2/soln.py
@@ -18,6 +18,20 @@ def is_valid_sequence(arr):
     return True
 
 
+def tolerate_single_bad(arr):
+    # first check if already valid
+    if is_valid_sequence(arr):
+        return True
+
+    n = len(arr)
+    for i in range(n):
+        test_arr = arr[:i] + arr[i+1:]
+        if is_valid_sequence(test_arr):
+            return True
+
+    return False
+
+
 def part1():
     with open('input.txt') as f:
         reports = f.read().strip().split('\n')
@@ -30,13 +44,44 @@ def part1():
 
     print(safe_report_count)
         
-    
+
+
+def part2():
+    with open('input.txt') as f:
+        reports = f.read().strip().split('\n')
+
+    single_bad = 0
+    for report in reports:
+        items = [int(i) for i in report.split(' ')]
+        if tolerate_single_bad(items):
+            single_bad += 1
+
+    print(single_bad)
+
+
 
 
 
 
 def main():
-    part1()
+    # arg parser
+    import argparse
+    parser = argparse.ArgumentParser(description="Aoc Day2")
+
+    parser.add_argument("--part1",action="store_true",help="Run part 1 of the day2")
+    parser.add_argument("--part2",action="store_true",help="Run Part2 of the day2")
+
+    args = parser.parse_args()
+
+    if not(args.part1 or args.part2):
+        print("Please provide flags")
+        return
+
+    if args.part1:
+        part1()
+    if args.part2:
+        part2()
+    
 
 
 if __name__ == '__main__':
